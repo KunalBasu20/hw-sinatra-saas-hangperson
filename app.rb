22329f4df0269a85f75c38897cc484a29a087782
Kunal Basu <kunal.basu@berkeley.edu>
2019-09-26 22:36:26 -0700

    Made some changes to edge case

--- app.rb
+++ app.rb
@@ -67,7 +67,7 @@ class HangpersonApp < Sinatra::Base
   end
   
   get '/win' do
-    if @game.check_win_or_lose == :win && @game.guesses.empty? && @game.wrong_guesses.empty?
+    if @game.check_win_or_lose == :win && @game.guesses.any? && @game.wrong_guesses.any?
       erb :win # You may change/remove this line
     else
       redirect '/show'
@@ -76,7 +76,7 @@ class HangpersonApp < Sinatra::Base
   end
   
   get '/lose' do
-    if @game.check_win_or_lose == :lose && @game.guesses.empty? && @game.wrong_guesses.empty?
+    if @game.check_win_or_lose == :lose && @game.guesses.any? && @game.wrong_guesses.any?
       erb :lose # You may change/remove this line
     else
       redirect '/show'
